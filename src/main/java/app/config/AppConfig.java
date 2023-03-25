1c7144416eca30352ee1baa55a5b8bdec830784e
micchecka <micchecka@mail.ru>
2023-03-25 23:05:57 +0300

    решено

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,23 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        return dog;
+    }
+
+    @Bean(name = "timer")
+    public Timer timer() {
+        Timer timer = new Timer();
+        return new Timer();
+    }
 }
